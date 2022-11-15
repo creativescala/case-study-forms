d56368db22e97cc58774104178dd5400361bba11
Noel Welsh <noel@noelwelsh.com>
2022-11-15 11:48:03 +0000

    Finish up validation
    - Correctly handle boolean inputs
    - Return validation state from rendering

--- web/src/main/scala/forms/Main.scala
+++ web/src/main/scala/forms/Main.scala
@@ -2,6 +2,7 @@ package forms
 
 import com.raquo.laminar.api.L._
 import org.scalajs.dom
+import forms.ValidationState._
 
 object Main {
   val appContainer: dom.Element = dom.document.querySelector("#appContainer")
@@ -15,7 +16,11 @@ object Main {
       }
     )
   val awesome = Field.boolean.withLabel("Is everything awesome?")
-  val question = weather.product(awesome)
+  val agree = Field.boolean
+    .withLabel("Do you agree that pizza is a type of toast?")
+    .withStyle(Style.boolean.choice("Yes", "No"))
+
+  val question = weather.product(awesome).product(agree)
   val form = Form("The Awesome Form", question)
 
   val appElement: HtmlElement = {
@@ -23,7 +28,27 @@ object Main {
     div(
       html,
       h2(className := "text-xl font-bold mb-2", "Submission"),
-      p(child <-- output.map(_.toString))
+      div(
+        child <-- output.map(vs =>
+          vs match {
+            case Invalid(_) => p("Come on, enter some valid input.")
+            case Unchecked =>
+              p("You need to enter input. You can't just leave it blank!")
+            case Valid(((weather, awesome), pizza)) =>
+              div(
+                p("You said:"),
+                ol(
+                  className := "list-disc m-2",
+                  li(s"The weather is $weather"),
+                  li(s"The awesomeness is set to ${awesome.toString()}"),
+                  li(
+                    s"You ${if (pizza) "do" else "do not"} agree that pizza is a type of toast."
+                  )
+                )
+              )
+          }
+        )
+      )
     )
   }
 
--- web/src/main/scala/forms/Render.scala
+++ web/src/main/scala/forms/Render.scala
@@ -1,21 +1,28 @@
 package forms
 
 import com.raquo.laminar.api.L._
+import scala.util.Random
 import forms.Field._
 import forms.Style.BooleanStyle._
 
 object Render {
-  def render[A](form: Form[A]): (HtmlElement, EventStream[A]) = {
+
+  /** Generate a random alphanumeric string */
+  def randomAlphanumeric(): String = Random.alphanumeric.take(8).mkString
+
+  def render[A](
+      form: Form[A]
+  ): (HtmlElement, EventStream[ValidationState[A]]) = {
     val (fieldsHtml, fieldsSignal) = renderField(form.fields)
     val submitted = new EventBus[Unit]
-    val output = new EventBus[A]
+    val output = new EventBus[ValidationState[A]]
     val html =
       div(
         className := "mb-4",
         h1(className := "text-4xl font-bold mb-4", form.title),
         fieldsHtml,
         button(
-          className := "ring-2 ring-sky-500 rounded-md border-2 border-sky-500 p-2 m-2 text-sky-500",
+          className := "ring-2 ring-sky-500 rounded-md border-2 border-sky-500 p-2 text-sky-500",
           typ := "submit",
           onClick.mapTo(()) --> submitted.writer,
           "Submit"
@@ -26,9 +33,11 @@ object Render {
     (html, output.events)
   }
 
-  def renderField[A](field: Field[A]): (HtmlElement, Signal[A]) = {
+  def renderField[A](
+      field: Field[A]
+  ): (HtmlElement, Signal[ValidationState[A]]) = {
     def renderLabel(lbl: Option[String]): HtmlElement =
-      div(className := "p-2", label(lbl))
+      div(label(lbl))
     def renderInput(input: HtmlElement): HtmlElement =
       div(className := "col-span-3", input)
     def renderValidation(
@@ -52,7 +61,7 @@ object Render {
         validationState: Signal[ValidationState[A]]
     ): HtmlElement =
       div(
-        className := "grid grid-cols-4 gap-x-2 gap-y-1 p-2",
+        className := "grid grid-cols-4 gap-x-2 gap-y-1",
         renderLabel(label),
         renderInput(input),
         renderValidation(validationState)
@@ -60,7 +69,9 @@ object Render {
 
     field match {
       case TextField(label, iV, ph, validation) =>
-        val state = Var(ValueState.uncommitted[String])
+        val state = Var(
+          iV.fold(ValueState.uncommitted[String])(v => ValueState.committed(v))
+        )
         val validationState = state.signal.map(state =>
           state match {
             case ValueState.Uncommitted => ValidationState.unchecked[String]
@@ -68,68 +79,92 @@ object Render {
               ValidationState.fromEither(validation(v))
           }
         )
-        val output = Var("")
         val commitValue =
           state.writer.contramap[String](v => ValueState.committed(v))
+
         val html =
           renderLabelAndInput(
             label,
-            div(
-              className := "col-span-3",
-              input(
-                typ := "text",
-                className := "ring-2 ring-sky-500 rounded-md border-2 border-sky-500 p-2",
-                // initial value takes precedence over placeholder
-                iV.map(iV => value := iV)
-                  .orElse(ph.map(ph => placeholder := ph)),
-                onInput.mapToValue --> output,
-                onInput.mapToValue --> state.writer.contramapOpt[String](v =>
-                  if (state.now().isUncommitted) None
-                  else Some(ValueState.committed(v))
-                ),
-                onBlur.mapToValue --> commitValue,
-                onChange.mapToValue --> commitValue
-              )
+            input(
+              typ := "text",
+              className := "ring-2 ring-sky-500 rounded-md border-2 border-sky-500 p-2",
+              // initial value takes precedence over placeholder
+              iV.map(iV => value := iV)
+                .orElse(ph.map(ph => placeholder := ph)),
+              onInput.mapToValue --> state.writer.contramapOpt[String](v =>
+                if (state.now().isUncommitted) None
+                else Some(ValueState.committed(v))
+              ),
+              onBlur.mapToValue --> commitValue,
+              onChange.mapToValue --> commitValue
             ),
             validationState.signal
           )
-        (html, output.signal)
+        (html, validationState)
 
       case BooleanField(style, lbl, iV) =>
-        val output = Var(iV.getOrElse(false))
+        val state = Var(
+          iV.fold(ValueState.uncommitted[Boolean])(v => ValueState.committed(v))
+        )
+        val validationState = state.signal.map(state =>
+          state match {
+            case ValueState.Uncommitted  => ValidationState.unchecked[Boolean]
+            case ValueState.Committed(v) => ValidationState.valid(v)
+          }
+        )
+        val commitValue =
+          state.writer.contramap[Boolean](v => ValueState.committed(v))
+
         val html =
           style match {
             case Checkbox =>
               renderLabelAndInput(
                 lbl,
-                div(
-                  className := "col-span-3",
-                  input(
-                    className := "mr-2 my-2 border-2 border-sky-500",
-                    typ := "checkbox",
-                    onClick.mapToChecked --> output
-                  )
+                input(
+                  className := "mr-2 my-2 border-2 border-sky-500",
+                  typ := "checkbox",
+                  onClick.mapToChecked --> commitValue
                 ),
-                Signal.fromValue(ValidationState.valid(output.now()))
+                validationState
               )
-            case Choice(trueChoice @ _, falseChoice @ _) =>
+            case Choice(trueChoice, falseChoice) =>
+              val n = randomAlphanumeric()
+              val trueId = randomAlphanumeric()
+              val falseId = randomAlphanumeric()
               renderLabelAndInput(
                 lbl,
-                div(
-                  className := "col-span-3",
-                  input(typ := "checkbox")
+                fieldSet(
+                  label(className := "pr-1", forId := trueId, trueChoice),
+                  input(
+                    typ := "radio",
+                    idAttr := trueId,
+                    name := n,
+                    onChange.mapTo(true) --> commitValue
+                  ),
+                  label(
+                    className := "pl-2 pr-1",
+                    forId := falseId,
+                    falseChoice
+                  ),
+                  input(
+                    typ := "radio",
+                    idAttr := falseId,
+                    name := n,
+                    onChange.mapTo(false) --> commitValue
+                  )
                 ),
-                Signal.fromValue(ValidationState.valid(output.now()))
+                validationState
               )
           }
 
-        (html, output.signal)
+        (html, validationState)
 
       case Product(left, right) =>
         val (leftHtml, leftOutput) = renderField(left)
         val (rightHtml, rightOutput) = renderField(right)
         val html = div(leftHtml, rightHtml)
-        val output = leftOutput.combineWith(rightOutput)
+        val output =
+          leftOutput.combineWith(rightOutput).map { case (l, r) => l.zip(r) }
 
         (html, output)
     }
